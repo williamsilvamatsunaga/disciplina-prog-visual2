03336aad554032a4579666cc3935ad11aa84c8f2
William da Silva Matsunaga <2023202060036@aluno.ifro.edu.br>
2026-08-05 21:05:30 -0400

    Primeira aula

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,19 @@
+﻿using MeuApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuApp.Controllers;
+
+public class AlunoController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        var alunos = new List<Aluno>()
+        {
+            new Aluno() { Id = 1, Nome = "William", Curso = "ADS", Nota = 10 },
+            new Aluno() { Id = 1, Nome = "William", Curso = "ADS", Nota = 10 },
+            new Aluno() { Id = 1, Nome = "William", Curso = "ADS", Nota = 10 }
+        };
+        return View(alunos);
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,9 @@
+﻿namespace MeuApp.Models;
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Curso { get; set; }
+    public double Nota { get; set; }
+}
